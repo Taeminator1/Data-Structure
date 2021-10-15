0e4146be2875754a9390599813cc9003e6b1fc8f
Taemin <dbsxoals689@naver.com>
2021-10-15 21:34:28 +0900

    Binary Tree 주석 추가

--- DataStructure/Binary Tree/BinaryNode.swift
+++ DataStructure/Binary Tree/BinaryNode.swift
@@ -7,15 +7,33 @@
 
 import Foundation
 
+
+/// A type is able to represent an element that consis of Binary Tree.
+///
+/// You can use this type with any type and set another instance to aproch that one, as in the following example:
+///
+///     let n1 = BinaryNode<Int>(1)
+///     let n2 = BinaryNode<Int>(3)
+///     let n3 = BinaryNode<Int>(2, left: n1, right: n2)
+///
+///     print(n3.right!.data)
+///     // Prints "3"
 public class BinaryNode<T> {
     public private(set) var data: T
-    public var left: BinaryNode?
-    public var right: BinaryNode?
+    public var left: BinaryNode?            // For left child node.
+    public var right: BinaryNode?           // For right child node.
     
+    /// Return the node is whether leaf or not.
     public var isLeaf: Bool {
         return left == nil && right == nil
     }
     
+    
+    /// Set the data want to store and the root of left or right subtree.
+    /// - Parameters:
+    ///   - data: Any kind of data.
+    ///   - left: A left root of subtree.
+    ///   - right: A right root of subtree.
     public init(_ data: T, left: BinaryNode<T>? = nil, right: BinaryNode<T>? = nil) {
         self.data = data
         self.left = left
--- DataStructure/Binary Tree/BinaryTree.swift
+++ DataStructure/Binary Tree/BinaryTree.swift
@@ -7,13 +7,20 @@
 
 import Foundation
 
+
+/// this type consist of node that is root.
 public class BinaryTree<Element> {
     public private(set) var root: BinaryNode<Element>?
     
+    /// A Boolean value indicating whether the collection is empty.
     public var isEmpty: Bool {
         return root == nil
     }
     
+    
+    /// The number of elements in the tree including root.
+    ///
+    /// - Complexity: O(*n*), where *n* is the number of the elements.
     public var count: Int {
         func recursiveFunction(binaryNode: BinaryNode<Element>?) -> Int {
             if let node = binaryNode {
@@ -27,6 +34,9 @@ public class BinaryTree<Element> {
         return recursiveFunction(binaryNode: root)
     }
 
+    /// The number of elements in the tree from root.
+    ///
+    /// - Complexity: O(*n*), where *n* is the number of the elements.
     public var height: Int {
         func recursiveFunction(binaryNode: BinaryNode<Element>?) -> Int {
             
@@ -43,7 +53,7 @@ public class BinaryTree<Element> {
     }
     
     public init() {
-        
+        self.root = nil
     }
     
     public init(root: BinaryNode<Element>) {
@@ -52,6 +62,24 @@ public class BinaryTree<Element> {
 }
 
 extension BinaryTree {
+    
+    
+    
+    /// Assemble the element of the tree by rouding.
+    /// Rounding in order like root of left subtree, root and root of right subtree.
+    ///
+    ///     let n1 = BinaryNode<Int>(1)
+    ///     let n2 = BinaryNode<Int>(3)
+    ///     let n3 = BinaryNode<Int>(2, left: n1, right: n2)
+    ///
+    ///     let t = BinaryTree<Int>(root: n3)
+    ///
+    ///     print(t.inorder())
+    ///     // Prints "[1, 2, 3]"
+    ///
+    /// - Returns: The array for the tree by inorder.
+    ///
+    /// - Complexity: O(*n*), where *n* is the number of the elements.
     public func inorder() -> [Element] {
         var arr: [Element] = []
         
@@ -67,6 +95,21 @@ extension BinaryTree {
         return arr
     }
     
+    /// Assemble the element of the tree by rouding.
+    /// Rounding in order like root, root of left subtree and root of right subtree.
+    ///
+    ///     let n1 = BinaryNode<Int>(1)
+    ///     let n2 = BinaryNode<Int>(3)
+    ///     let n3 = BinaryNode<Int>(2, left: n1, right: n2)
+    ///
+    ///     let t = BinaryTree<Int>(root: n3)
+    ///
+    ///     print(t.inorder())
+    ///     // Prints "[2, 1, 3]"
+    ///
+    /// - Returns: The array for the tree by preorder.
+    ///
+    /// - Complexity: O(*n*), where *n* is the number of the elements.
     public func preorder() -> [Element] {
         var arr: [Element] = []
         
@@ -82,6 +125,21 @@ extension BinaryTree {
         return arr
     }
     
+    /// Assemble the element of the tree by rouding.
+    /// Rounding in order like root of left subtree, root of right subtree and root.
+    ///
+    ///     let n1 = BinaryNode<Int>(1)
+    ///     let n2 = BinaryNode<Int>(3)
+    ///     let n3 = BinaryNode<Int>(2, left: n1, right: n2)
+    ///
+    ///     let t = BinaryTree<Int>(root: n3)
+    ///
+    ///     print(t.inorder())
+    ///     // Prints "[1, 3, 2]"
+    ///
+    /// - Returns: The array for the tree by postorder.
+    ///
+    /// - Complexity: O(*n*), where *n* is the number of the elements.
     public func postorder() -> [Element] {
         var arr: [Element] = []
         
